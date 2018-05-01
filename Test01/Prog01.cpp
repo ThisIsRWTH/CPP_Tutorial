66b8a9b86085705834bb4db486f8140a07c50a22
Mahdi Saber <mysoodonims@gmail.com>
2018-05-01 17:34:12 +0200

    initialization

---
+++ Test01/Prog01.cpp
@@ -0,0 +1,17 @@
+/*
+ * Prog01.cpp
+ *
+ *  Created on: Apr 20, 2018
+ *      Author: SoftStar
+ */
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout<<"This is my first prog!";
+	return 0;
+}
+
